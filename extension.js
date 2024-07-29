56ba707134fc6691df24e2e739ca2be2c5057c7c
Dimitar Rupov <dimitar.rupov@gmail.com>
2024-07-29 10:36:09 +0200

    Add remaining minutes

--- extension.js
+++ extension.js
@@ -14,14 +14,16 @@ const TimerIndicator = GObject.registerClass(
         'org.gnome.shell.extensions.x-min-beep@drupov'
       )
       this._interval = this._settings.get_int('interval')
+      this._remainingTime = this._interval
 
       this.label = new St.Label({
-        text: `${this._interval} min beep`,
+        text: `${this._interval} min beep (${this._remainingTime})`,
         y_align: Clutter.ActorAlign.CENTER,
       })
       this.add_child(this.label)
 
       this._timeout = null
+      this._countdownTimeout = null
 
       this._playBeep = () => {
         let [success, argv] = GLib.shell_parse_argv(
@@ -32,17 +34,45 @@ const TimerIndicator = GObject.registerClass(
         }
       }
 
+      this._updateLabel = () => {
+        this.label.text = `${this._interval} min beep (${this._remainingTime})`
+      }
+
+      this._startCountdown = () => {
+        if (this._countdownTimeout) {
+          GLib.source_remove(this._countdownTimeout)
+        }
+        this._remainingTime = this._interval
+        this._updateLabel()
+        this._countdownTimeout = GLib.timeout_add_seconds(
+          GLib.PRIORITY_DEFAULT,
+          60,
+          () => {
+            this._remainingTime--
+            this._updateLabel()
+            if (this._remainingTime > 0) {
+              return true
+            }
+            return false
+          }
+        )
+      }
+
       this.connect('button-press-event', () => {
         if (this._timeout) {
           GLib.source_remove(this._timeout)
+          GLib.source_remove(this._countdownTimeout)
           this._timeout = null
+          this._countdownTimeout = null
           this.label.remove_style_class_name('active')
         } else {
+          this._startCountdown()
           this._timeout = GLib.timeout_add_seconds(
             GLib.PRIORITY_DEFAULT,
             this._interval * 60,
             () => {
               this._playBeep()
+              this._startCountdown()
               return true
             }
           )
@@ -52,14 +82,17 @@ const TimerIndicator = GObject.registerClass(
 
       this._settings.connect('changed::interval', () => {
         this._interval = this._settings.get_int('interval')
-        this.label.text = `${this._interval} min beep`
+        this._remainingTime = this._interval
+        this._updateLabel()
       })
     }
 
     destroy() {
       if (this._timeout) {
         GLib.source_remove(this._timeout)
+        GLib.source_remove(this._countdownTimeout)
         this._timeout = null
+        this._countdownTimeout = null
       }
       super.destroy()
     }
